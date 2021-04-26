80640dd77fbf5baca18759db4472052391e40c77
pbouras <44644709+pbouras@users.noreply.github.com>
2021-04-27 01:03:07 +0300

    Update SVMClassifier.py

--- SVMClassifier.py
+++ SVMClassifier.py
@@ -14,9 +14,6 @@ test_set.to_csv("my.csv", encoding='utf-8')
 y_test = test_set['Winners']
 y_train = train_set['Winners']
 y_valid = valid_test['Winners']
-#y_valid = valid_test['Winners']
-#X_train = train_set[['Year','Team1GP','WIN%_x','Team1PTS','Team1FGA','Team1FG%','Team13PA','Team13P%','Team1FTA','Team1FT%','Team1REB','Team1AST','Team1TOV','Team1STL','Team1BLK','Team1BLKA','Team1PF','Team1+/-','Team2GP','WIN%_y','Team2PTS','Team2FGA','Team2FG%','Team23PA','Team23P%','Team2FTA','Team2FT%','Team2REB','Team2AST','Team2TOV','Team2STL','Team2BLK','Team2BLKA','Team2PF','Team2+/-']]
-#X_test = test_set[['Year','Team1GP','WIN%_x','Team1PTS','Team1FGA','Team1FG%','Team13PA','Team13P%','Team1FTA','Team1FT%','Team1REB','Team1AST','Team1TOV','Team1STL','Team1BLK','Team1BLKA','Team1PF','Team1+/-','Team2GP','WIN%_y','Team2PTS','Team2FGA','Team2FG%','Team23PA','Team23P%','Team2FTA','Team2FT%','Team2REB','Team2AST','Team2TOV','Team2STL','Team2BLK','Team2BLKA','Team2PF','Team2+/-']]
 X_train = train_set[['Year','Team1FGA','Team1FG%','Team13PA','Team13P%','Team1REB','Team1TOV','Team1STL','Team2FGA','Team2FG%','Team23PA','Team23P%','Team2REB','Team2TOV','Team2STL']]
 X_test = test_set[['Year','Team1FGA','Team1FG%','Team13PA','Team13P%','Team1REB','Team1TOV','Team1STL','Team2FGA','Team2FG%','Team23PA','Team23P%','Team2REB','Team2TOV','Team2STL']]
 X_valid = valid_test[['Year','Team1FGA','Team1FG%','Team13PA','Team13P%','Team1REB','Team1TOV','Team1STL','Team2FGA','Team2FG%','Team23PA','Team23P%','Team2REB','Team2TOV','Team2STL']]
@@ -30,12 +27,12 @@ clf = SVC(gamma='scale',probability=True)
 clf.fit(X_train, y_train)
 clf.score(X_test,y_test)
 
-y_pred = clf.predict(X_valid)
+y_pred = clf.predict(X_test)
 
 pd.DataFrame(y_pred).to_csv("final.csv")
-'''
+
 print(f1_score(y_test,y_pred))
 print(accuracy_score(y_test,y_pred))
 cm = confusion_matrix(y_test,y_pred)
 print(cm)
-'''
+
